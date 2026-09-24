61aaa5bb4a057960de2df157a5de9b5ab537d23d
GGP404 <217957946+GGP404@users.noreply.github.com>
2026-09-24 10:30:45 +0300

    library lab1

---
+++ OnlineShop.Common/Phone.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Common
+{
+    public class Phone : Product
+    {
+        public string Brand { get; set; }
+        public int Memory { get; set; }
+        public string OperatingSystem { get; set; }
+
+        // Constructor
+        public Phone(
+            string name,
+            double price,
+            string brand,
+            int memory,
+            string operatingSystem)
+            : base(name, price)
+        {
+            Brand = brand;
+            Memory = memory;
+            OperatingSystem = operatingSystem;
+        }
+
+        // Method
+        public override void ShowInfo()
+        {
+            Console.WriteLine(
+                $"Телефон: {Brand} {Name}, {Memory} GB, {Price} грн");
+        }
+    }
+}
