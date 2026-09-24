08a99536b31cd47b4eb1032dc51533b8224262b0
GGP404 <217957946+GGP404@users.noreply.github.com>
2026-09-24 10:38:31 +0300

    Add files via upload

---
+++ OnlineShop/OnlineShop.Common/CrudService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Common
+{
+    public class CrudService<T> : ICrudService<T>
+        where T : Product
+    {
+        private List<T> items = new List<T>();
+
+        // Create
+        public void Create(T element)
+        {
+            items.Add(element);
+        }
+
+        // Read
+        public T Read(Guid id)
+        {
+            return items.First(x => x.Id == id);
+        }
+
+        // Read All
+        public IEnumerable<T> ReadAll()
+        {
+            return items;
+        }
+
+        // Update
+        public void Update(T element)
+        {
+            var oldElement = items.First(x => x.Id == element.Id);
+
+            int index = items.IndexOf(oldElement);
+
+            items[index] = element;
+        }
+
+        // Remove
+        public void Remove(T element)
+        {
+            items.Remove(element);
+        }
+    }
+}
---
+++ OnlineShop/OnlineShop.Common/Laptop.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Common
+{
+    public class Laptop : Product
+    {
+        public string Brand { get; set; }
+        public int Ram { get; set; }
+        public string Processor { get; set; }
+
+        // Constructor
+        public Laptop(
+            string name,
+            double price,
+            string brand,
+            int ram,
+            string processor)
+            : base(name, price)
+        {
+            Brand = brand;
+            Ram = ram;
+            Processor = processor;
+        }
+
+        // Method
+        public override void ShowInfo()
+        {
+            Console.WriteLine(
+                $"Ноутбук: {Brand} {Name}, RAM: {Ram} GB, {Price} грн");
+        }
+    }
+}
